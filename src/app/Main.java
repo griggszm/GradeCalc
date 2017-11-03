316b157f22fad3e1e9f43401a475f646cfe25197
griggszm <griggszm@msoe.edu>
2017-11-02 23:48:50 -0500

    Initial design created

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("design.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 500, 475));
+        primaryStage.setScene(new Scene(root, 850, 775));
         primaryStage.show();
     }
 
